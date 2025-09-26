662a74b8b8151ed598247fdfcfea96f0f1eea8bd
Katyag17 <ekgaidachuk@gmail.com>
2025-09-26 18:45:33 -0400

    Changed the place of the initialized models and added admin jobpost moderation

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from .models import JobPost
 # Register your models here.
+@admin.register(JobPost)
+class JobPostAdmin(admin.ModelAdmin) :
+    list_display = ("title", "created_by", "approved", "created_at")
+    list_filter = ("approved", "created_at")
+    search_fields = ("title", "description")
+
+    actions = ["approve_posts", "reject_posts"]
+
+    def approve_posts(self, request, queryset):
+        queryset.update(approved=True)
+    approve_posts.short_description = "Approve selected job posts"
+
+    def reject_posts(self, request, queryset):
+        queryset.update(approved=False)
+    reject_posts.short_description = "Reject selected job posts"
--- home/models.py
+++ home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+#jobpost model
+class JobPost(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
